b04deb4de0679e830dca059f1a564f3693935c3a
Tevin Hlebela <tevinhlebela@gmail.com>
2023-09-08 19:53:59 +0200

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /*This program prints the sizes of various data types*/
 int main(void)
 {
-//This function prints out the datatypes
+/*This function prints out the datatypes*/
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
